0c47adcdea91a05a4f089f8d531213f209362045
Jo Colina <jo@jocolina.com>
2016-10-14 16:01:00 +0200

    Bug fixes

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 var fs = require('fs');
 var verbs = JSON.parse(fs.readFileSync('./data.json'));
 
+var fishingrod = require('fishingrod');
+
 var mysql = require('mysql');
 var connection = mysql.createConnection({
 	host: 'localhost',
@@ -15,7 +17,7 @@ var cheerio = require('cheerio');
 var times = ['indicatif', 'futur', 'imparfait', 'passe-simple','sub-pres', 'sub-imparfait', 'conditionnel', 'imperatif']
 var personnes = ['je', 'tu', 'il/elle', 'nous', 'vous', 'ils'];
 
-var reg = /([^\s]*)$/i;
+var reg = /([^\s']*)$/i;
 
 function getVerb(index){
 	if(!verbs[index]){
@@ -34,22 +36,31 @@ function getVerb(index){
 		fishingrod.fish({
 			https: false,
 			host: 'conjugateur.fr',
-			path: `/conjuguer-verbe-${verb}`,
+			path: `/conjuguer-verbe-${verbs[v_index]}`,
 			method: 'GET'
 		}, function(st, res){
 			var $ = cheerio.load(res);
 			var conjug = $('.conjug');
 
-			for(var i = 0; i < 6; i++){
+			for(var i = 0; i <7; i++){
 				for(var j = 0; j < 6; j++){
-					var conjug = conjug[j].innerText;
+					if(!conjug[j] || !conjug[j].firstChild){
+						continue;
+					}
+					var conjug = conjug[j].firstChild.data + conjug[j].firstChild.next.firstChild.data;
 					console.log('Adding', conjug, '...');
-					insertConjug(conjug, verb.id, personnes[j], times[i], v_index);
+					insertConjug(reg.exec(conjug)[1], id, personnes[j], times[i]);
 				}
 			}
 
+			continueparsing(v_index);
+
 		});
 	});
+}
+
+function continueparsing(v_index){
+	getVerb(v_index+1);
 }
 
 function insertVerb(index, callback){
@@ -58,11 +69,11 @@ function insertVerb(index, callback){
 		if(err){
 			return console.error('Error inserting verb', err);
 		}
-		callback(fields.insertId, index);
+		callback(rows.insertId, index);
 	});
 }
 
-function insertConjug(conjug, verb_id, person, time, v_index){
+function insertConjug(conjug, verb_id, person, time){
 	var query = 'INSERT INTO conjugated VALUES(null, ?, ?, ?, ?)';
 	connection.query(query, [verb_id, conjug, person, time], function(err, rows, fields){
 		if(err){
@@ -70,7 +81,7 @@ function insertConjug(conjug, verb_id, person, time, v_index){
 		}
 
 		console.log('SUCCESSFULLY ADDED', conjug);
-		getVerb(vindex+1);
 	})
 }
 
+getVerb(0);
